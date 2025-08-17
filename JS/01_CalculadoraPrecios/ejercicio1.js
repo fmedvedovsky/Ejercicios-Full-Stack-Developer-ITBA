0c02f0aa1f57fb262c0164322a2d78fcb2ace215
Fernando David Medvedovsky <fmedvedovsky@gmail.com>
2025-08-17 19:49:46 -0300

    Ejercicio 1 de JS

---
+++ JS/01_CalculadoraPrecios/ejercicio1.js
@@ -0,0 +1,8 @@
+const precioBaseSilla = 4500.5;
+const iva = 0.21;
+let valorIva = 4500.5 * 0.21;
+let precioFinal = precioBaseSilla + valorIva;
+
+console.log("Precio base de la silla: $" + precioBaseSilla);
+console.log("IVA: $" + valorIva);
+console.log("Precio final: $" + precioFinal);
