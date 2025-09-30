34141592ff6191146cdbd981889773b4781608ce
Fernando David Medvedovsky <fmedvedovsky@gmail.com>
2025-09-30 00:15:45 -0300

    Ejercicio 2 react

--- REACT_01/mi-primera-app/src/App.js
+++ REACT_01/mi-primera-app/src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import TarjetaPresentacion from './TarjetaPresentacion';
 
 function App() {
   return (
     <div className="App">
       <h1>Mi Primera App en React</h1>
       <p>FDM</p>
+
+
+      <TarjetaPresentacion />
+      <TarjetaPresentacion />
+      <TarjetaPresentacion />
+
     </div>
   );
 }
---
+++ REACT_01/mi-primera-app/src/TarjetaPresentacion.js
@@ -0,0 +1,11 @@
+function TarjetaPresentacion() {
+    return (
+        <div>
+            <h2>Ada Lovelace</h2>
+            <p>Programadora y Matemática</p>
+            <p>Considerada la primera programadora de la historia</p>
+        </div>
+    );
+}
+
+export default TarjetaPresentacion;
