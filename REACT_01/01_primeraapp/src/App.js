48d852cd7e185ccee44eaa1e932f0726142ac732
Fernando David Medvedovsky <fmedvedovsky@gmail.com>
2025-09-30 00:02:27 -0300

    Ejercicio 1 de React

---
+++ REACT_01/01_primeraapp/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Mi Primera App en React</h1>
+      <p>FDM</p>
+    </div>
+  );
+}
+
+export default App;
